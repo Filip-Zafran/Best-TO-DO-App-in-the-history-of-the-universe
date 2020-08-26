f764e32fafa8aea5e89db52c63bb258966db128e
FichoZee <filip.zafran@gmail.com>
2020-08-26 23:31:26 +0200

    create Todo.js component and  create a list with onclick line-through decoration.   - add VisibilityFilters to actions

--- src/actions/index_act.js
+++ src/actions/index_act.js
@@ -6,3 +6,11 @@ export const addTodo = text => ({
     text
 
 })
+
+export const VisibilityFilters = {
+
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,9 +1,37 @@
 // this is a smart component
 import { connect } from 'react-redux';
 import TodoList from '../components/TodoList';
+import {VisibilityFilters, toggleTodo} from '../actions'
+
+const getVisibleTodos = (todos, filter) => {
+
+    switch (filter) {
+
+        case VisibilityFilters.SHOW_ALL:
+            return todos;
+        case VisibilityFilters.SHOW_COMPLETED:
+            return todos.filter(task => task.completed);
+        case VisibilityFilters.SHOW_ACTIVE:
+            return todos.filter(task => !task.completed);
+        
+        default:
+            throw new Error('Unknown filter: ' + filter);
+        
+    }
+
+}
+
+
+const mapStateToProps = state => ({
+todos: getVisibleTodos(state.todos, state.visibilityFilter)
+
+});
+
+
 
 
 export default connect(
-
+    mapStateToProps,
+    mapDispatchToProps
     
 )(TodoList);
