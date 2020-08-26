73e628b801db422ad201d3fadeee012ab3d3c342
FichoZee <filip.zafran@gmail.com>
2020-08-26 14:19:41 +0200

    initialize index.js / Provider, store and rootReducer

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Provider and createStore enables connecting state to the redux app
+
+// Provider is react-redux connects all the states and makes them accessable from all the areas 
+import { Provider } from 'react-redux';
+// createStore combines all states into 1 file, and we can acess the store from any component 
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+// rootReducer is addition of all the reducers we have into one file
+ const store = createStore(rootReducer)  
+ 
+
+
 ReactDOM.render(
-  <React.StrictMode>
+
+  // this is how you make all the states from the tore accesible in all components in the app
+  // cause its now in the main 'component' and its easy to just pull data from here down 
+<Provider store= { store }>
+
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
