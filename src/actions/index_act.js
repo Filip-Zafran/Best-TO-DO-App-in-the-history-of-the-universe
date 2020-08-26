41229009f07c76be51e9c52dfa0d7a4cfadd0080
FichoZee <filip.zafran@gmail.com>
2020-08-26 23:12:36 +0200

    create actions index and export addTodo

---
+++ src/actions/index_act.js
@@ -0,0 +1,8 @@
+let nextTodoId = 0;
+
+export const addTodo = text => ({
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+
+})
--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { addTodo } from '../actions';
 
 const AddTodo = ( ) => {
 
@@ -12,7 +12,19 @@ const AddTodo = ( ) => {
     
         <div>
             
-            <form>
+             <form
+                onSubmit={
+                    e => {
+                        e.preventDefault();
+                        
+            if (!input.value.trim()){
+                     return
+                        }
+                        
+                     dispatch(addTodo(input.value));
+                        input.value = '';
+                            }
+             }>
             
                 <input type="text" ref={el => (input = el)} />
                 <button type="submit"> Add Todo test</button>
